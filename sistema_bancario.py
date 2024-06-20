c1d9c8f14b91f5f730b201547305f658f7d6a542
MironNeto <miron.neto@hotmail.com>
2024-06-20 20:06:49 -0300

    feat: previsão de novas situações
    Adição de previsões de mal uso e/ou situacoes nao tratadas anteriormente, como caso haja depositos ou saques negativos.

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -9,7 +9,7 @@ Escolha a sua operacao desejada:
 """
 
 saldo = 0
-extrato = "______________EXTRATO______________\n\n"
+extrato = ""
 numero_saques = 0
 LIMITE_SAQUES_DIARIOS = 3
 VALOR_LIMITE_SAQUE = 500
@@ -24,10 +24,13 @@ Opcao deposito selecionada.
 Digite o valor que voce deseja depositar.
 """
         valor_deposito = float(input(mensagem))
-        saldo += valor_deposito
+        if(valor_deposito <= 0):
+            print("Valor do deposito deve ser positivo.")
 
-        extrato += f'Deposito  -------------    R$ {valor_deposito:.2f}\n'
-        print(f"Depositado valor de R$ {valor_deposito:.2f} na conta.")
+        else:
+            saldo += valor_deposito
+            extrato += f'Deposito  -------------    R$ {valor_deposito:.2f}\n'
+            print(f"Depositado valor de R$ {valor_deposito:.2f} na conta.")
         
     elif opcao == "s":        
         mensagem = """
@@ -36,12 +39,18 @@ Digite o valor que voce deseja sacar.
 """
         valor_saque = float(input(mensagem))
 
-        if valor_saque > VALOR_LIMITE_SAQUE:
+        if valor_saque <= 0:
+            print("Valor do saque deve ser positivo.")
+
+        elif valor_saque > VALOR_LIMITE_SAQUE:
             print("Valor solicitado esta acima do valor limite por saque.")
+
         elif valor_saque > saldo:
             print("Saldo insuficiente.")
+
         elif numero_saques >= LIMITE_SAQUES_DIARIOS:
             print("Limite de saques diarios atingido")
+
         else:
             saldo -= valor_saque
             numero_saques += 1
@@ -51,8 +60,16 @@ Digite o valor que voce deseja sacar.
 
 
     elif opcao == "e":
-        print("Opcao extrato selecionada.\n\n")
-        print(extrato)
+        print("______________EXTRATO______________")
+        print("___________________________________")
+        
+        print("Nao foram realizadas movimentacoes" if not extrato else extrato)
+
+        print(f'Saldo     -------------    R$ {saldo:.2f}')
+
+        print("___________________________________")
+
+
     elif opcao == "q":
         print("Opcao sair selecionada.")
         break
